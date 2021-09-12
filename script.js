f3726935b3d4c0de352c46bfc98fd3fe449f9cc8
Adam <a.6942@yahoo.com>
2021-09-12 15:45:20 -0400

    added window prompts

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 // Assignment code here
-
-
+var length = window.prompt ("Please enter desired length for password");
+var lowercase = window.confirm ("Would you like to include lowercase characters?");
+var uppercase = window.confirm ("Would you like to include uppercase characters?");
+var numeric = window.confirm ("Would you like to include numeric characters?");
+var special = window.confirm ("Would you like to include special characters?");
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
