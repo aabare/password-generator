c8a1e5cfe39b81da72015bcdec14cc5637c103b9
aabare <88077451+aabare@users.noreply.github.com>
2021-09-13 21:24:46 -0400

    Added for loops and rearranged code

--- assets/script.js
+++ assets/script.js
@@ -1,72 +1,66 @@
 // Assignment code here
 var generateBtn = document.querySelector("#generate");
 
-//Variables
-var lower = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',];
+ // Write password to the #password input
+ function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
 
-var upper = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',];
-
-var numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9',];
-
-var specials = ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '<', '>', '?', '/', '+', '-',]
-
-
-//Event Listener
-generateBtn.addEventListener("click", generatePassword)
-
-
+  passwordText.value = password;
+}
 //Window Prompts
 function generatePassword() {
 
-  var password = "";
-  var passwordChar = "";
-  var restart = function() {
     var length = window.prompt ("Please enter desired length for password");
-    length = parseInt(length);
     if (length >=8 && length <=128) {
       
     } else {
-      confirm("Please enter a value between 8 and 128");
-      restart();
-    }};
-    restart();
+      alert("Please choose a value between 8 and 128");
+      return;
+    }
+
+  var choices = [];
+  var lower = "abcdefghijklmnopqrstuvwxyz";
+  var upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  var numbers = "1234567890";
+  var specials = "!@#$%^&*()-_+=<>?/,.";
   //Lowercase Selector  
     var lowercase = window.confirm ("Would you like to include lowercase characters?");
-      if (lowercase) {
-        passwordChar += lower;
+      if (lowercase ===  true) {
+        for (var i = 0; i < lower.length; i++) {
+          choices.push(lower[i]);
+        }
       }
-
     //Uppercase Selector
     var uppercase = window.confirm ("Would you like to include uppercase characters?");
-      if (uppercase) {
-        passwordChar += upper;
+    if (uppercase ===  true) {
+      for (var i = 0; i < upper.length; i++) {
+        choices.push(upper[i]);
       }
-
+    }
     //Numeric Selector
     var numeric = window.confirm ("Would you like to include numeric characters?");
-        if (numeric) {
-          passwordChar += numbers;
-        }
-
+    if (numeric ===  true) {
+      for (var i = 0; i < numbers.length; i++) {
+        choices.push(numbers[i]);
+      }
+    }
     //Special Selector
     var special = window.confirm ("Would you like to include special characters?");
-          if (special) {
-          passwordChar += specials; 
-          }
-      
-    //For Loop
+    if (special ===  true) {
+      for (var i = 0; i < specials.length; i++) {
+        choices.push(specials[i]);
+      }
+    }
+    //For Loop 
+    var newPassword = "";
           for (var i = 0; i < length; i++) {
-        password = passwordChar [Math.floor(Math.random() * passwordChar.length)]
+            choices[Math.floor(Math.random() * choices.length)];
+            newPassword +=
+              choices[Math.floor(Math.random() * choices.length)];
       }
       
- // Write password to the #password input
-  function writePassword() {
-    var password = generatePassword();
-    var passwordText = document.querySelector("#password");
-  
-    passwordText.value = password;
-  }
+          return newPassword;
+    }
 
   generateBtn.addEventListener("click", writePassword)
-
-  }
